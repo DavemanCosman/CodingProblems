b14aef8e8a642344e67a721161a3068a4a06e3e5
David Cosman <davemancosman@gmail.com>
2020-12-09 16:49:36 -0500

    Update mutateTheArray.java
    Added comments with explanation

--- mutateTheArray.java
+++ mutateTheArray.java
@@ -1,6 +1,19 @@
+/* David C. - mutateTheArray problem from CodeSignal
+ ******************************************************
+ * Mutate a given array a of defined length n into a
+ * new array b of length n, as follows:
+ * * b[i] = a[i - 1] + a[i] + a[i + 1]
+ * * if the element at that index i doesn't exist,
+ *   replace with 0.
+ ******************************************************
+ * Returns:
+ * * the mutated array b
+ *****************************************************
+ */
 int[] mutateTheArray(int n, int[] a) {
     int b [] = new int [n];
     for (int i = 0 ; i<n ; i++){
+	// using ternary operators makes the code short and efficient
         b[i] = (i!=0 ? a[i-1] : 0) + a[i] + (i!=n-1 ? a[i+1] : 0);
     }
     return b;
