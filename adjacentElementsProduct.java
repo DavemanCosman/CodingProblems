5f451beef0e9fc2d072e374eda6b08efeadfd5a9
David Cosman <davemancosman@gmail.com>
2020-12-09 18:57:53 -0500

    Update adjacentElementsProduct.java

--- adjacentElementsProduct.java
+++ adjacentElementsProduct.java
@@ -1,3 +1,13 @@
+/* David C. - adjacentElementsProduct problem from CodeSignal
+ ******************************************************
+ * Given an array of integers, find the pair of
+ * adjacent elements that has the largest product and 
+ * return that product.
+ ******************************************************
+ * Returns:
+ * largest product of adjecent elements
+ *****************************************************
+ */
 int adjacentElementsProduct(int[] inputArray) {
     int largest = inputArray[0] * inputArray[1]; //First and second positions
     for (int i = 1 ; i < inputArray.length-1 ; i ++){
